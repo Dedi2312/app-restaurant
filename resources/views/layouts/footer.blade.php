e7819e18f7e15cb133401e719ac6d7f599b74772
Dedi2312 <dediputrawan2312@gmail.com>
2024-05-31 16:12:41 +0800

    apps 1

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer bg-dark">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 1.0
+    </div>
+    <strong>Copyright &copy; 2024 <a href="#">Restaurant</a>.</strong> All rights reserved.
+  </footer>
